7fbfeed703aed1f806f1fcc2a5666b9149706dc7
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-08-30 15:04:50 +0800

    Fix Browser Router bug

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './css/navbar.css'
 import './css/discipleshipguide.css'
 import './css/main.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
