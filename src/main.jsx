98aeac4e8d748bdf6a539ca762c12339efaaedcc
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-08-30 16:32:41 +0800

    try fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import './css/navbar.css'
 import './css/discipleshipguide.css'
 import './css/main.css'
