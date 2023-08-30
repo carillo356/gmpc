e86d27a1c7c7af3b81428b5a256d7ff27f81fb37
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-08-30 15:13:26 +0800

    Test Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,8 +22,8 @@ function NavBar() {
           <span className='website-name'>Logo Here</span>
           <button className='nav-btn' onClick={handleClick}><FaBars className='nav-icon'/></button>
           <nav className={isOpen ? 'nav-links' : 'hide'}>
-              {/* <Link to="/gmpc/" onClick={() => setIsMenuOpen(false)}>Start Here</Link> */}
-              <Link to="/gmpc/guide" onClick={() => setIsMenuOpen(false)}>Discipleship Guide</Link>
+              <Link to="/gmpc/guide" onClick={() => setIsMenuOpen(false)}>Start Here</Link>
+              <Link to="/gmpc/" onClick={() => setIsMenuOpen(false)}>Discipleship Guide</Link>
           </nav>
       </header>
     </div>
@@ -31,13 +31,14 @@ function NavBar() {
     <div className={isOpen ? 'mobile-nav-links' : 'hide'}>
         <nav>
             <button className='nav-btn' onClick={handleClick}><FaTimes className='nav-icon'/></button>
-            {/* <Link to="/gmpc/" onClick={() => setIsMenuOpen(false)}>Start Here</Link> */}
-            <Link to="/gmpc/guide" onClick={() => setIsMenuOpen(false)}>Discipleship Guide</Link>
+            <Link to="/gmpc/guide" onClick={() => setIsMenuOpen(false)}>Start Here</Link>
+            <Link to="/gmpc/" onClick={() => setIsMenuOpen(false)}>Discipleship Guide</Link>
         </nav>
     </div>
 
     <Routes>
         <Route path="/gmpc/" element={<DiscipleshipGuide />}/>
+        <Route path="/gmpc/guide" element={<Home />}/>
     </Routes>
     
   </>
