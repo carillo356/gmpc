76a8749776aa1f1b1d7753d34fe69c670194afe4
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-08-30 16:14:02 +0800

    try fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './css/navbar.css'
 import './css/discipleshipguide.css'
 import './css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/gmpc/'>
       <App />
-    </HashRouter>
-  </React.StrictMode>,
+    </BrowserRouter>
+  </React.StrictMode>
 )
